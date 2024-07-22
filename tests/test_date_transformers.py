a4ebc39f4d88919d3d8ab102bb89487ad816896b
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2024-07-22 16:47:51 +0100

    get trasnformer tests working

---
+++ tests/test_date_transformers.py
@@ -0,0 +1,46 @@
+import datetime as dt
+from unittest import mock
+
+from ..helpers.constants import C_SCHOOLDAY_CUTOFF_HOUR
+from ..helpers.DateCheckers import (Transform1Hour,
+                                    TransformNextDay,
+                                    TransformAfterSchoolOr1Hour,
+                                    )
+
+
+class TestDateTransformers:
+
+    def test_transform_1_hour(self):
+
+        objID = Transform1Hour()
+        test_dt = dt.datetime(year=2024, month=1, day=1, hour=12, minute=3)
+        test_new_dt = objID.transform(test_dt)
+
+        assert (test_new_dt-test_dt).total_seconds() == (60*60)
+
+    def test_transform_next_day(self):
+
+        objID = TransformNextDay()
+        test_dt = dt.datetime(year=2024, month=1, day=1, hour=12, minute=3)
+        test_new_dt = objID.transform(test_dt)
+
+        assert test_new_dt.day == 2
+        assert test_new_dt.hour == 9
+    
+    def test_after_school(self):
+
+        objID = TransformAfterSchoolOr1Hour()
+        test_dt = dt.datetime(year=2024, month=1, day=1, hour=12)
+        test_new_dt = objID.transform(test_dt)
+
+        assert test_new_dt.day == 1
+        assert test_new_dt.hour == C_SCHOOLDAY_CUTOFF_HOUR
+
+    def test_after_school_by_1_hour(self):
+
+        objID = TransformAfterSchoolOr1Hour()
+        test_dt = dt.datetime(year=2024, month=1, day=1, hour=C_SCHOOLDAY_CUTOFF_HOUR)
+        test_new_dt = objID.transform(test_dt)
+
+        assert test_new_dt.day == 1
+        assert test_new_dt.hour == C_SCHOOLDAY_CUTOFF_HOUR+1
